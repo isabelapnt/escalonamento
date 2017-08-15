21c0fa125f4645cbfb15116ed8070ef7b4f3f5f4
Felipe Borges <felipeborgez@gmail.com>
2017-08-15 17:09:18 -0300

    Corrige entradas de teste e o calculo do turnaround do FIFO.

--- root.py
+++ root.py
@@ -1,6 +1,7 @@
 from __future__ import division
 from time import sleep
 from threading import Thread
+# from queue import Queue
 
 
 class Processo(Thread):
@@ -9,7 +10,8 @@ class Processo(Thread):
 		self.id = id
 		self.tempo_chegada = tempo_chegada
 		self.tempo_execucao = tempo_execucao
- 
+ 	
+
 	def run(self):
 		print '\nProcesso %d: Chegando em %d' % (self.id, self.tempo_chegada)
 		sleep(self.tempo_execucao)
@@ -18,17 +20,43 @@ class Processo(Thread):
 
 
 
+class Scheduler(object):
+	
+	def __init__(self):
+		self.cpu = None
+		self.processes = []
+
+	def add(self, process):
+		self.processes.append(process)
+
+	def pop(self):
+		return self.process.pop()
+
+	
+	def start():
+		for p in self.processes:
+			p.start()
+
+	def is_busy():
+		pass
+		
+		
+
+
+
 def fifo(processos):
+	index = 0
+	time_in = 0 
 	turnaround = 0
 	size = len(processos)
 
 	processos.sort(key= lambda p: p['tempo_chegada'])
-	index = 0
 	print 'Comecando execucao!\n'
 
 	t = None
 	for p in processos:
-		turnaround += p['tempo_execucao'] - p['tempo_chegada']
+		time_in = p['tempo_execucao'] + time_in
+		turnaround += time_in - p['tempo_chegada']
 		sleep(p['tempo_chegada'])
 		t = Processo(index, p['tempo_chegada'], p['tempo_execucao'])
 		t.setName(index)
@@ -39,12 +67,13 @@ def fifo(processos):
 	print '\n\nTurnaround: '+ str(turnaround / size)
 	# Gerar grafico
 
+
 def _sjf(process):
 	index = 0
 	time_in = 0 
 	turnaround = 0
-
 	size = len(process)
+
 
 	process.sort(key= lambda p: p['tempo_chegada'])
 	first = process[0]
@@ -56,7 +85,7 @@ def _sjf(process):
 
 	for p in range_process:
 		time_in = p['tempo_execucao'] + time_in
-		turnaround += time_in + - p['tempo_chegada']
+		turnaround += time_in - p['tempo_chegada']
 		sleep(p['tempo_chegada'])
 		t = Processo(index, p['tempo_chegada'], p['tempo_execucao'])
 		t.setName(index)
@@ -82,7 +111,9 @@ if __name__ == "__main__":
 
 	print 'Informe os dados dos processos:'
 	while 0 < opcao_algoritmo  < 6:
-		if opcao_algoritmo == 1: #FIFO
+		
+		#FIFO
+		if opcao_algoritmo == 1:
 			processos = list()
 			for i in range(0, quantidade_processos):
 				print '\nProcesso %d:' % i
@@ -92,6 +123,7 @@ if __name__ == "__main__":
 			fifo(processos)
 			break
 
+		
 		if opcao_algoritmo == 2:
 			processos = list()
 			for i in range(0, quantidade_processos):
